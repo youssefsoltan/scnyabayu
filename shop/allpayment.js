e03fbd38a767e8e7e047f139f9928dcbdf152a73
BayuuOFC <nandoardipradana123@gmail.com>
2022-01-29 14:14:13 +0700

    scnyabayu

---
+++ shop/allpayment.js
@@ -0,0 +1,36 @@
+const allpayment = (prefix) => {
+return `┏━━━━━━━━━━━━━━━━━━━━
+┃                  *PAYMENT*
+┣━━━━━━━━━━━━━━━━━━━━
+┣━━━⊱❉ *🏧 BANK* ❉⊰━━━━✿
+┃  
+┣━⊱ *BRI*
+┣⊱ BRI [TIDAK TERSEDIA]
+┣━✿ NAMA BRI [TIDAK TERSEDIA]
+┗━━━━━━━━━━━━━━━━━━━━
+
+┏━━━━━━━━━━━━━━━━━━━━
+┃                  *PAYMENT*
+┣━━━━━━━━━━━━━━━━━━━━
+┣━━⊱❉ *💰 E-WALLET* ❉⊰━━━✿
+┃   
+┣━⊱ *GOPAY*
+┣⊱ 0895 2551 8325
+┣━━✿ NAMA GOPAY ${ownername}
+┃
+┣━⊱ *DANA*
+┣⊱ 0812 8586 6502
+┣━━✿ NAMA DANA ${ownername}
+┃
+┣━⊱ *PULSA*
+┣⊱ 0812 8586 6502
+┣━━✿ NAMA PULSA ${ownername}
+┃
+┗━━━━━━━━━━━━━━━━━━━━
+
+JANGAN LIAT² DOANG DONASI BENERAN DONG 🗿
+`
+	}
+
+exports.allpayment = allpayment
+ 
